8ea2fafcf8a54b16cb791992561fd3139350e1c8
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-05-09 02:29:37 +0300

    Create Generic.cs

---
+++ Generic.cs
@@ -0,0 +1,26 @@
+//Author:SercanCelenk
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericIfadeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> AsalSayilar = new List<int>();
+            AsalSayilar.Add(2);
+            AsalSayilar.Add(3);
+            AsalSayilar.Add(5);
+
+            Console.WriteLine(AsalSayilar[1]);
+            Console.ReadLine();
+
+
+        }
+    }
+}
